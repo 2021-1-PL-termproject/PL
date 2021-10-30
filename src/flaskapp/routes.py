89d1a21de907d0eb76905724dfc83e7b3f4ca39f
DESKTOP-3BN3JKG\jinjh <bangjh0730@yonsei.ac.kr>
2021-10-30 13:35:19 +0900

    Change routing

--- src/flaskapp/routes.py
+++ src/flaskapp/routes.py
@@ -9,29 +9,10 @@ def main():
         usrid=request.form["ownerid"]
         return redirect(url_for("user", usr=usrid))
     else:
-        return render_template('index.html')
+        return render_template('login.html')
 
 @app.route('/<usr>')
 def user(usr):
     if type(get_csv(usr)) == str:
         return 'No User'
-    stm = mean_time_of_state(usr, '수면')
-    sts = std_time_of_state(usr, '수면')
-    atm = mean_time_of_state(usr, '기상하기')
-    ats = std_time_of_state(usr, '기상하기')
-    avs = angle_to_hms(angle_dif(atm, stm))
-
-    rmm = mean_time_of_state(usr, '일반 약 복용')
-    rms = std_time_of_state(usr, '일반 약 복용')
-
-    toilet = average_num_use(usr, '화장실 이용')
-    hobby = average_num_use(usr, '취미활동')
-
-    bm = mean_time_of_state(usr, '조식')
-    bs = std_time_of_state(usr, '조식')
-    lm = mean_time_of_state(usr, '중식')
-    ls = std_time_of_state(usr, '중식')
-    dm = mean_time_of_state(usr, '석식')
-    ds = std_time_of_state(usr, '석식')
-
-    return render_template('user.html', usr=usr, stm=stm, sts=sts, atm=atm, ats=ats, avs=avs, rmm=rmm, rms=rms, toilet=toilet, hobby=hobby, bm=bm, bs=bs, lm=lm, ls=ls, dm=dm, ds=ds)
+    return render_template('main.html', usr=usr)
