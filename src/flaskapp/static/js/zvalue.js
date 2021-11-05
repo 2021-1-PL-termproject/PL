ff1894225ba114f23529cdf71825e7b6be4b32a3
Shin <sds544@naver.com>
2021-11-05 20:17:36 +0900

    c

--- src/flaskapp/static/js/zvalue.js
+++ src/flaskapp/static/js/zvalue.js
@@ -1,35 +1,28 @@
-var A;
-var B;
-var C;
-var D;
-const ctx = document.getElementById('myChart');
-const myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['부동', '미동', '활동', '매우 활동'],
-        datasets: [{
-            label: '활동량',
-            data: [A, B, C, D],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(75, 192, 192, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(75, 192, 192, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
+
+google.charts.load('current', {packages: ['corechart', 'bar']});
+google.charts.setOnLoadCallback(drawMultSeries);
+
+function drawMultSeries() {
+      var data = google.visualization.arrayToDataTable([
+        ['활동성', '감지'],
+        ['부동', A],
+        ['미동', B],
+        ['활동', C],
+        ['매우 활동', D],
+      ]);
+
+      var options = {
+        title: '활동량 점수',
+        chartArea: {width: '50%'},
+        hAxis: {
+          title: 'Total Population',
+          minValue: 0
+        },
+        vAxis: {
+          title: 'City'
         }
+      };
+
+      var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+      chart.draw(data, options);
     }
-});
